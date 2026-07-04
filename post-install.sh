4eebd7fd59e01b9c9a145eac2e6396e05307ce9a
hoicau <69333371+hoicau@users.noreply.github.com>
2026-07-05 01:30:51 +0800

    add naiveproxy binary and remove usteer

--- post-install.sh
+++ post-install.sh
@@ -2,6 +2,3 @@
 sed -i 's|downloads\.immortalwrt\.org|immortalwrt.kyarucloud.moe|g' /etc/apk/repositories.d/distfeeds.list
 # Set up OpenWrt-momo
 curl -sSfL https://github.com/nikkinikki-org/OpenWrt-momo/raw/refs/heads/main/feed.sh | ash
-# Set up NaiveProxy with sing-box (libcronet.so)
-curl -L -o /usr/bin/libcronet.so "https://github.com/SagerNet/cronet-go/releases/latest/download/libcronet-linux-arm64.so" \
-&& chmod 0755 /usr/bin/libcronet.so
